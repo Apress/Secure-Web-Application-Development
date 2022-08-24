e5aa4c74b9da59c9f694613d58857c29b7891f49
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2022-08-24 14:35:11 -0400

    Delete coffeeshopsite directory

--- coffeeshopsite/coffeeshop/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-from .models import *
-
-
-class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Address
-        fields = ['pk', 'address1', 'address2', 'city', 'postcode', 'country']
--- coffeeshopsite/loaddata.sh
+++
@@ -1,7 +0,0 @@
-. /secrets/config.env
-cd /var/www/coffeeshopsite
-python3 manage.py loaddata coffeeshop/fixtures/product.json
-python3 manage.py loaddata coffeeshop/fixtures/address.json
-python3 manage.py loaddata coffeeshop/fixtures/card.json
-python3 manage.py loaddata coffeeshop/fixtures/stocklevel.json
-
